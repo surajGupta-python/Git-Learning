5d810f6c96a16a611d4d7f4f69817e1f22f07fc9
Suraj <agentgupta216@gmail.com>
2026-06-20 12:49:44 +0530

    flatten arr func

---
+++ dummy.py
@@ -0,0 +1,15 @@
+nested_list = [2, 4, [12, 5], [10, [9, 11]], 17]
+
+
+
+def flatten_list(arr):
+    flatten_arr = []
+    for ele in arr:
+        if isinstance(ele, list):
+            flatten_arr.extend(flatten_list(ele))
+        else:
+            flatten_arr.append(ele)
+    return flatten_arr
+
+result = flatten_list(nested_list)
+print(result)
